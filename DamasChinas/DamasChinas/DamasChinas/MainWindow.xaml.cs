e9b277512414d1c5256d5e7a6f49cffac5415333
Nelo95 <carballo2809@hotmail.com>
2017-09-24 21:46:02 -0500

    Se agregaron ventanas para internacionalizar
    Se agrego el 40% de las ventanas del proyecto para iniciar su internacionalización

---
+++ DamasChinas/DamasChinas/DamasChinas/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DamasChinas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ResourceManager AdministradorDeRecursos;
+        CultureInfo Cultura;
+        string Lenguaje;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AdministradorDeRecursos = new ResourceManager("DamasChinas.lenguaje.Resources", typeof(MainWindow).Assembly);
+            Lenguaje = "en-US";
+            PonerTexto();
+        }
+        //Cambiar el texto de sus respectivos textox,botones
+        private void PonerTexto()
+        {
+            Cultura = CultureInfo.CreateSpecificCulture(Lenguaje);
+            Usuario.Text = AdministradorDeRecursos.GetString("User", Cultura);
+            Contraseña.Text = AdministradorDeRecursos.GetString("Pass", Cultura);
+            Inicio_sesion.Content = AdministradorDeRecursos.GetString("Login", Cultura);
+            Idioma.Text = AdministradorDeRecursos.GetString("Language", Cultura);
+            Esp.Text = AdministradorDeRecursos.GetString("Spanish", Cultura);
+            Eng.Text = AdministradorDeRecursos.GetString("English", Cultura);
+            Pregunta.Text = AdministradorDeRecursos.GetString("NotUser", Cultura);
+            Registro.Text = AdministradorDeRecursos.GetString("Register", Cultura);
+
+         
+        }
+
+        private void Regitrarse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Registrarse registro = new Registrarse();
+            PonerTexto();
+            registro.Show();
+            Close();
+        }
+
+        private void Esp_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Lenguaje = "es-MX";
+            PonerTexto();
+        }
+
+        private void Eng_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Lenguaje = "en-US";
+            PonerTexto();
+        }
+    }
+}
